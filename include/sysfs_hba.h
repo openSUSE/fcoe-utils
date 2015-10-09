7ccdeabb3c6cf4d6c7ae36d887192e3f09261d43
Johannes Thumshirn <jthumshirn@suse.de>
2015-10-09 19:12:24 +0000

    fcoe-utils: Add sysfs_hba to fcoemon_utils
    Add sysfs_hba to to fcoemon_utils. This is the 1st part of obsoleting
    libHBAAPI2 and libhbalinux2 for fcoe-utils.
    
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Vasu Dev <vasu.dev@intel.com>

---
+++ include/sysfs_hba.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright(c) 2015 SUSE GmbH. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * Maintained at www.Open-FCoE.org
+ */
+
+#ifndef _SYSFS_HBA_H
+#define _SYSFS_HBA_H
+
+#include <stdint.h>
+
+#define SYSFS_HOST_DIR	"/sys/class/fc_host"
+#define SYSFS_NET_DIR	"/sys/class/net"
+
+#define HBA_PORTSPEED_4GBIT		0x0008  /* 4 GBit/sec */
+#define HBA_PORTSPEED_8GBIT		0x0010  /* 8 GBit/sec */
+#define HBA_PORTSPEED_16GBIT		0x0020  /* 16 GBit/sec */
+#define HBA_PORTSPEED_32GBIT		0x0040  /* 32 GBit/sec */
+#define HBA_PORTSPEED_20GBIT		0x0080  /* 20 GBit/sec */
+#define HBA_PORTSPEED_40GBIT		0x0100  /* 40 GBit/sec */
+#define HBA_PORTSPEED_NOT_NEGOTIATED	(1 << 15) /* Speed not established */
+
+struct port_attributes {
+	char device_name[256];
+	char symbolic_name[256];
+	char node_name[256];
+	char port_name[256];
+	char fabric_name[256];
+	char speed[256];
+	char supported_speeds[256];
+	char maxframe_size[256];
+	char port_id[256];
+	char port_state[256];
+};
+
+struct hba_info {
+	char manufacturer[64];
+	char serial_number[64];
+	char model_description[256];
+	char hardware_version[256];
+	char driver_name[256];
+	char driver_version[256];
+	uint32_t nports;
+};
+
+
+int get_number_of_adapters(void);
+struct hba_info *get_hbainfo_by_pcidev(const char *pcidev);
+struct port_attributes *get_port_attribs(const char *host);
+char *get_pci_dev_from_netdev(const char *netdev);
+char *get_host_from_netdev(const char *netdev);
+
+#endif /* _SYSFS_HBA_H */
